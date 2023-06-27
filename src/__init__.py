b5355ecffcebb5b01f2c2289a120b06f04507e18
alfonso <alfonso1003@users.noreply.github.com>
2023-06-27 11:33:39 -0600

    set up for data persistence

--- src/__init__.py
+++ src/__init__.py
@@ -1,12 +1,20 @@
 from flask import Flask
 
 from src.api import api_bp
+from src.extensions import db, ma
 
 
 def create_app() -> Flask:
     app = Flask(__name__)
     app.config.from_object("src.config.DevelopmentConfig")
 
+    register_extensions(app)
+
     app.register_blueprint(api_bp, url_prefix="/api")
 
     return app
+
+
+def register_extensions(app: Flask):
+    db.init_app(app)
+    ma.init_app(app)
--- src/config.py
+++ src/config.py
@@ -1,14 +1,17 @@
 class Config:
     TESTING = False
+    SECRET_KEY = "secretkey!"  # Don't do this in real app!
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 class DevelopmentConfig(Config):
-    pass
+    SQLALCHEMY_DATABASE_URI = "salite:///dev-db.sqlite"
 
 
 class TestingConfig(Config):
     TESTING = True
+    SQLALCHEMY_DATABASE_URI = "salite:///test-db.sqlite"
 
 
 class ProductionConfig(Config):
-    pass
+    SQLALCHEMY_DATABASE_URI = "salite:///prod-db.sqlite"
