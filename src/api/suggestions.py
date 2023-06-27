f838473d7a86ad47ffe18a6e776db0deee8cbce3
alfonso <alfonso1003@users.noreply.github.com>
2023-06-27 15:45:53 -0600

    make endpoint

--- src/api/suggestions.py
+++ src/api/suggestions.py
@@ -1,4 +1,7 @@
-from flask_restx import Namespace, Resource, reqparse
+from flask import request
+from flask_restx import Namespace, Resource
+
+from src.cities.services import get_suggestions
 
 api = Namespace(
     "suggestions", description="Provide auto-complete suggestions for large cities"
@@ -6,8 +9,28 @@ api = Namespace(
 
 
 class SuggestionList(Resource):
-    def post(self):
+    @api.doc(
+        params={
+            "name": {
+                "description": "Enter a name for a city",
+                "type": "str",
+                "default": "New York",
+            },
+            "lat": {
+                "description": "Enter a latitude",
+                "type": "float",
+                "default": 39.5,  # geographic center of us
+            },
+            "long": {
+                "description": "Enter a longitude",
+                "type": "float",
+                "default": 98.35,  # geographic center of us
+            },
+        }
+    )
+    def get(self):
         """Get a list of auto-complete suggestions based on parameters"""
+        return get_suggestions()
 
 
 api.add_resource(SuggestionList, "")
