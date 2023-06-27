fa63bf689cfb0ae3c1628d875f85611c2a1151a9
alfonso <alfonso1003@users.noreply.github.com>
2023-06-27 15:46:31 -0600

    set up stub for service layer.... fill in business logic later!

--- src/cities/services.py
+++ src/cities/services.py
@@ -1,3 +1,11 @@
 from src import db
 from src.cities.models import City
 from src.cities.schemas import city_list_schema
+
+
+def get_suggestions():
+    # TODO: fun part! business logic
+    # name similarity use haversine distance
+    # to get distances, use geoalchemy2 and coordinates
+
+    return city_list_schema.dump(City.query.limit(10).all()), 200
